673cc90712afc2246a79530c92736b89e44685a5
Garrett Heinlen <heinleng@gmail.com>
2014-08-16 16:16:10 +1000

    fixing 404 page (I don’t know why this doesn’t load templates?)

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule PhoenixCrud.PageController do
   use Phoenix.Controller
 
   def show(conn, %{"page" => "unauthorized"}) do
-    render conn, "unauthorized"
+    # [g] idk why this template won't render?
+    # render conn, "unauthorized"
+
+    html conn, """
+      <html>
+        <head>
+          <title>404 Page not found.</title>
+        </head>
+        <body>
+          <h1> Page not found! </h1>
+        </body>
+      </html>
+    """
   end
 end
